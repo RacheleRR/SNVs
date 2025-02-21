87e6d717d9b6a42981c618f97a184982b79baab6
RacheleRR <124870594+RacheleRR@users.noreply.github.com>
2025-02-21 14:22:11 +0100

    Add files via upload

---
+++ Scripts/vcf_to_tsv/vcf_to_tsv_less/filter_tab_vcf.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import argparse
+import re
+
+# Argument parser
+parser = argparse.ArgumentParser(description='Filter TSV file.')
+parser.add_argument('-i', '--input', required=True, help='Input TSV file')
+parser.add_argument('-o', '--output', required=True, help='Output TSV file')
+args = parser.parse_args()
+
+input_tsv = args.input
+output_tsv = args.output
+
+info_columns = [
+    "#CHROM", "POS", "REF", "ALT", "Allele", "pLI_gene_value", "IMPACT", "SYMBOL", 
+    "Consequence", "MPC_score", "MVP_score", "gnomADg_AF", "gnomADe_AF", 
+    "am_pathogenicity", "SIFT_pred", "CADD_phred", "GERP___RS"
+]
+
+# Load the TSV file into a pandas DataFrame
+df = pd.read_csv(input_tsv, sep='\t', low_memory=False)
+
+# Check if column names contain indices and remove them if they do
+if any(re.search(r'\[\d+\]', col) for col in df.columns):
+    df.columns = [re.sub(r'\[\d+\]', '', col).strip() for col in df.columns]
+
+# Filter the DataFrame to keep only the desired columns that exist in the DataFrame
+existing_columns = [col for col in info_columns if col in df.columns]
+filtered_df = df[existing_columns].copy()
+
+# Rename the '#CHROM' column to 'CHROM'
+filtered_df.rename(columns={'#CHROM': 'CHROM'}, inplace=True)
+
+# Keep only unique rows
+filtered_df.drop_duplicates(inplace=True)
+
+# Save the filtered DataFrame to a new TSV file
+filtered_df.to_csv(output_tsv, sep='\t', index=False)
+
+print("TSV data successfully filtered and saved to new TSV file.")
---
+++ Scripts/vcf_to_tsv/vcf_to_tsv_less/import_vcf.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Ensure the input file is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 -i input_file"
+    exit 1
+fi
+
+input_file=""
+while getopts "i:" opt; do
+    case $opt in
+        i) input_file=$OPTARG ;;
+        *) echo "Usage: $0 -i input_file"; exit 1 ;;
+    esac
+done
+
+# Define the output file
+output_file="${input_file%.vcf}_all_fields_output.tsv"
+
+# Run the bcftools command and check for errors
+if ! bcftools +split-vep "$input_file" -c "CSQ" -HH -f "%CHROM\t%POS\t%REF\t%ALT\t%CSQ\n" -d -A tab -o "$output_file"; then
+    echo "Error: bcftools command failed."
+    exit 1
+fi
+
+echo "VCF file has been transformed to a tab-separated file with all fields."
